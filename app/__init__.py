0a51d6984c8a41a6c7a2db1e50bf3faacc045cf1
Shubhrajit1pallob <spallob@asu.edu>
2025-02-14 21:45:50 -0700

    Added the requirements

--- app/__init__.py
+++ app/__init__.py
@@ -10,3 +10,11 @@ def create_app():
         app.register_blueprint(main_blueprint)
 
     return app
+
+# app = Flask(__name__)
+# app.secret_key = "Hi1/tanI/sinx"
+
+# with app.app_context():
+#     # Import routes here to avoid circular imports
+#     from .routes import main as main_blueprint
+#     app.register_blueprint(main_blueprint)
